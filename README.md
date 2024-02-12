b188017080623f08225e03b71a2d589f51a21756
David Chang <david.chang@takeoff.com>
2024-02-12 14:25:41 -0500

    Dockerize the very basic Go web app

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # dev-davidchang-api
 RESTful API for interacting with David Chang programmatically
 
+## Build & Run
+### Build
+`$ docker build -t dev-davidchang-api .`
+### Run
+`$ docker run -p 8080:8080 -it --rm --name dev-dc-api dev-davidchang-api`
+
 ## API Design (https://api.davidchang.dev)
 >[!NOTE]
->_The `resumes` endpoint saves and retrieves resumes (binary data) from object storage_
+>_The `resumes` endpoint retrieves and saves resumes (binary data) from/to object storage_
 ### `/resumes` 
 - GET - Get a list of resumes, returned as JSON
-- POST - Add a new resume from request data sent as ???
+- POST - Add a new resume from request data sent as JSON
 ### `/resumes:id` 
-- GET - Get a resume by ID, returning resume data as ???
+- GET - Get a resume by ID, returning resume data as JSON
 
 >[!NOTE]
 >_The `family-members` endpoint saves and retrieves family member info saved in a relational (?) or document (?) database_
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+// resume struct represents data about a resume
+type resume struct {
+	ID	string	`json:"id"`
+	Year	int	`json:"year"`
+	URL	string	`json:"URL"`
+}
+
+// resume slice to seed resume data.
+var resumes = []resume{
+	{ID: "1", Year: 2019, URL: "https://docs.google.com/document/d/1tiFMVTbrzlbHnxUusnT8veVlejbiZpts0dHW1GlJGf8/edit?usp=drive_link"},
+	{ID: "2", Year: 2021, URL: "https://docs.google.com/document/d/1RQd595CTc8MBs6GSLChokZXpfXnN4eS-P4KCB0qpazo/edit?usp=drive_link"},
+	{ID: "3", Year: 2023, URL: "https://docs.google.com/document/d/1RQd595CTc8MBs6GSLChokZXpfXnN4eS-P4KCB0qpazo/edit?usp=drive_link"},
+	{ID: "4", Year: 2024, URL: "https://davidchang.dev"},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "https://docs.davidchang.dev")
+	})
+	router.GET("/resumes", getResumes)
+
+	router.Run(":8080")
+}
+
+// getResumes responds with the list of all resumes as JSON.
+func getResumes(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, resumes)
+}
